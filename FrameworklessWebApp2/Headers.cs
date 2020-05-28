36b8268b8321ce48e6e3c637854060632ef8f71b
Amanda <amandachau36@gmail.com>
2020-05-28 12:10:28 +1000

    change header to json

--- FrameworklessWebApp2/Headers.cs
+++ FrameworklessWebApp2/Headers.cs
@@ -12,7 +12,7 @@ namespace FrameworklessWebApp2
         {
             var headers = new Dictionary<string, string>  // can make this into a factory if I require different combos
             {
-                {"Content-Type", "text/html"}, 
+                {"Content-Type", "application/json"}, 
             };
             
             foreach (var (key, value) in headers)
